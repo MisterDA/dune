ocaml/jbuilder

Export C/C++ compiler found in Cxx_flags
Signed-off-by: Antonin Décimo <antonin@tarides.com>

src/dune_rules/cxx_flags.mli
@@ -4,10 +4,17 @@ open! Stdune
 
 open Dune_engine
 
+(** The detected compiler *)
+type ccomp_type =
+  | Gcc
+  | Msvc
+  | Clang
+  | Other of string
+
 (** The name of the file created in the .dune folder after calling the C
     preprocessor *)
 val preprocessed_filename : string
 
 (** [get_flags c_compiler] returns the necessary flags to turn this compiler
     into a c++ compiler for some of the most common compilers *)
-val get_flags : Path.Build.t -> string list Build.t
+val get_flags : Path.Build.t -> (ccomp_type * string list) Build.t

MisterDA/dune

src/dune_rules/cxx_flags.mli
@@ -4,10 +4,17 @@ open! Stdune
 
 open Dune_engine
 
+(** The detected compiler *)
+type ccomp_type =
+  | Gcc
+  | Msvc
+  | Clang
+  | Other of string
+
 (** The name of the file created in the .dune folder after calling the C
     preprocessor *)
 val preprocessed_filename : string
 
 (** [get_flags c_compiler] returns the necessary flags to turn this compiler
     into a c++ compiler for some of the most common compilers *)
-val get_flags : Path.Build.t -> string list Action_builder.t
+val get_flags : Path.Build.t -> (ccomp_type * string list) Action_builder.t